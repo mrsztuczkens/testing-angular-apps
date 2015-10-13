c2cc644df01adf6a44e15005da31e99c110313c2
Marcin Skrzypiec <mrsztuczkens@gmail.com>
2015-10-13 19:39:36 +0200

    Added comments to controller spec

--- test/spec/controllers/todoListControllerSpec.js
+++ test/spec/controllers/todoListControllerSpec.js
@@ -3,10 +3,10 @@ describe('todoListController', function(){
 	var $controller;
 	
 	beforeEach(function(){
-		angular.mock.module('todoApp');
+		angular.mock.module('todoApp'); //initialize our module
 		
 		angular.mock.inject(function(_$controller_){
-			$controller = _$controller_('todoListController');
+			$controller = _$controller_('todoListController'); //create an instance of the todoListController
 		});
 	});
 	
