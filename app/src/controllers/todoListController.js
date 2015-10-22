216c984d90484a4d33bacc17fb89387049befd59
Marcin Skrzypiec <mrsztuczkens@gmail.com>
2015-10-22 19:59:40 +0200

    Updates for part 2

--- app/src/controllers/todoListController.js
+++ app/src/controllers/todoListController.js
@@ -3,13 +3,65 @@
 	
 	angular.module("todoApp").controller("todoListController", todoListController);
 		
-	todoListController.$inject = [];
+	todoListController.$inject = ['$filter', 'todos'];
 		
-	function todoListController(){
+	function todoListController($filter, todos){
 		
-		this.test = true;
-		this.anArray = ['item1', 'item2'];
+		var vm = this;
 		
+		vm.cancel = cancel;
+		vm.current = {};
+		vm.currentId = -1;
+		vm.edit = edit;
+		vm.editing = false;
+		vm.save = save;
+		vm.select = select;
+		vm.todos = todos || [];
+		
+		init();
+		
+		function cancel(){
+			var matches = $filter('filter')(vm.todos, { id : vm.currentId }, true);
+			vm.current = matches[0];
+			vm.editing = false;
+		}
+		
+		function edit(){
+			if (!vm.editing){
+				vm.editing = true;
+			}
+		}
+		
+		function init() {
+			if (vm.todos.length > 0){
+				vm.current = angular.copy(vm.todos[0]);
+				vm.currentId = vm.current.id;
+			}
+		}
+		
+		function save(){
+			if (!vm.editing)
+				return;
+				
+			for(var i = 0; i < vm.todos.length; i++){
+				if (vm.todos[i].id == vm.currentId){
+					vm.todos[i] = angular.copy(vm.current);
+					break;
+				}
+			}
+			vm.editing = false;
+		}
+		
+		function select(id){
+			var matches = $filter('filter')(vm.todos, { id : id }, true);
+			if (matches.length > 0){
+				vm.current = angular.copy(matches[0]);
+				vm.currentId = vm.current.id;
+			} else {
+				vm.current = {};
+				vm.currentId = -1;
+			}
+		}
 	}
 	
 })();
--- test/spec/controllers/todoListControllerSpec.js
+++ test/spec/controllers/todoListControllerSpec.js
@@ -1,17 +1,76 @@
 describe('todoListController', function(){
 	
 	var $controller;
+	var testTodos = [
+		{ id: 1, title: 'Todo #1', description: 'Description of todo 1' },	
+		{ id: 2, title: 'Todo #2', description: 'Description of todo 2' },	
+	]; 
 	
 	beforeEach(function(){
 		angular.mock.module('todoApp'); //initialize our module
 		
 		angular.mock.inject(function(_$controller_){
-			$controller = _$controller_('todoListController'); //create an instance of the todoListController
+			$controller = _$controller_('todoListController', {
+				todos: angular.copy(testTodos)
+			}); //create an instance of the todoListController
 		});
 	});
 	
-	it('should work', function(){
-		chai.expect($controller.test).to.be.true;
-		chai.expect($controller.anArray).to.have.lengthOf(2);
+	it('should initialize everything', function(){
+		chai.expect($controller.editing).to.be.false;
+		chai.expect($controller.todos).to.have.length(testTodos.length, 'Should have all the todos that were passed');
+		chai.expect($controller.currentId).to.equal(1, 'currentId should point to the first todo');
+		chai.expect($controller.current).to.deep.equal(testTodos[0], "Current should hold the first todo");
+	});
+	
+	describe('select item', function(){
+		
+		beforeEach(function(){
+			$controller.select(2);
+		});
+		
+		it('should set currentId to 2', function(){
+			chai.expect($controller.currentId).to.equal(2);
+		});
+		
+		it('should copy the second todo to current', function(){
+			chai.expect($controller.current).to.deep.equal(testTodos[1]);
+		});
+		
+		describe('and click edit', function(){
+			
+			beforeEach(function(){
+				$controller.edit();
+			});
+			
+			it('should set editing to true', function(){
+				chai.expect($controller.editing).to.be.true;
+			});
+			
+			describe('and edit item', function(){
+				
+				var updatedTodo = { id: 2, title: 'Brand new title', description: 'Brand new description' };
+				
+				beforeEach(function(){
+					$controller.current = angular.copy(updatedTodo);
+				});
+				
+				it('should update todos and set editing to false after saving', function(){
+					$controller.save();
+					
+					chai.expect($controller.editing).to.be.false;
+					chai.expect($controller.todos[1]).to.deep.equal(updatedTodo);
+				});
+				
+				it('should restore the current value and set editing to false after canceling', function(){
+					$controller.cancel();
+					
+					chai.expect($controller.editing).to.be.false;
+					chai.expect($controller.current).to.deep.equal(testTodos[1]);
+				});
+				
+			});
+		});
+		
 	});
 });
